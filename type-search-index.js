46268dcbd9175c81430f24df200250176a41f879
Takahiko Kawasaki <taka@authlete.com>
2023-06-22 02:06:57 +0900

    Updated JavaDoc for version 1.3.

--- type-search-index.js
+++ type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.authlete.sd","l":"Disclosure"},{"p":"com.authlete.sd","l":"SDJWT"},{"p":"com.authlete.sd","l":"SDObjectBuilder"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.authlete.sd","l":"Disclosure"},{"p":"com.authlete.sd","l":"SDJWT"},{"p":"com.authlete.sd","l":"SDObjectBuilder"},{"p":"com.authlete.sd","l":"SDObjectDecoder"},{"p":"com.authlete.sd","l":"SDObjectEncoder"}]
