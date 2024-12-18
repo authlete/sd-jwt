6c78f321414153f39600e6bc9ed77ffd835c18bb
Takahiko Kawasaki <taka@authlete.com>
2024-12-18 23:49:52 +0900

    Updated JavaDoc for version 1.5.

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.authlete.sd"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.authlete.sd"}];updateSearchResults();
